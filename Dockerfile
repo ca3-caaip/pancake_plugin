dce2a3d19f91ca3f61794be911b347edcf4fe123
regonn <programmer@haizine.art>
2022-06-07 06:21:04 +0000

    feat: swap

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,2 @@
-FROM python:3
+FROM python:3.9
 USER root
-
-RUN pip install web3
-RUN pip install eth-event
-RUN pip install etherscan-python
-RUN pip install git+https://github.com/ca3-caaip/senkalib\#egg=senkalib -t /senkalib
-RUN export PYTHONPATH="./senkalib:$PYTHONPATH"
--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
-from setuptools import setup, find_packages
-from glob import glob
+# -*- coding: utf-8 -*-
+from setuptools import setup
 
-setup(
-    name='pancake_plugin',
-    version='0.0.1',
-    license='mit',
-    description='plugin for pancake',
+package_dir = {"": "src"}
 
-    author='ca3-caaip',
-    author_email='',
-    url='https://github.com/ca3-caaip/ca3-caaip',
+packages = ["pancake_plugin"]
 
-    packages=find_packages('src'),
-    package_dir={'': 'src'}
-)
+package_data = {"": ["*"]}
+
+install_requires = [
+    "pandas>=1.4.2,<2.0.0",
+    "senkalib @ "
+    "git+https://github.com/ca3-caaip/senkalib.git@b4a41ab228f2532707f712eb11ae68da9aa1b378",
+]
+
+setup_kwargs = {
+    "name": "pancake-plugin",
+    "version": "0.1.0",
+    "description": "plugin for PancakeSwap",
+    "long_description": None,
+    "author": "ca3-caaip",
+    "author_email": None,
+    "maintainer": None,
+    "maintainer_email": None,
+    "url": None,
+    "package_dir": package_dir,
+    "packages": packages,
+    "package_data": package_data,
+    "install_requires": install_requires,
+    "python_requires": ">=3.9,<4.0",
+}
+
+
+setup(**setup_kwargs)
